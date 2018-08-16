c59046fc3bab6840d1e08df05b75b7f2c4bafb1e
aayushdutt <aayushd100@gmail.com>
2018-08-17 03:55:12 +0530

    add env vars in prodn keys

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,15 +1,15 @@
 module.exports = {
-  "token": "",
-  "calendarId": "",
+  "token": process.env.token,
+  "calendarId": process.env.calendarId,
   "credentials": {
     "installed": {
-      "client_id": "",
-      "project_id": "",
-      "auth_uri": "",
-      "token_uri": "",
-      "auth_provider_x509_cert_url": "",
-      "client_secret": "",
-      "redirect_uris": ["", ""]
+      "client_id": process.env.client_id,
+      "project_id": process.env.project_id,
+      "auth_uri": process.env.auth_uri,
+      "token_uri": process.env.token_uri,
+      "auth_provider_x509_cert_url": process.env.auth_provider_x509_cert_url,
+      "client_secret": process.env.client_secret,
+      "redirect_uris": [process.env.redirect_uris1, process.env.redirect_uris2]
     }
   }
 }
